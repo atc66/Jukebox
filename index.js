f1a6ff394628f631049e1e9b54229896d57a4df6
aciukurescu <andrewciukurescu@gmail.com>
2018-01-24 12:44:42 -0500

    created vars

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('test')
+
+
+var box = getElementById('box')
+var play = getElementById('play')
+var pause = getElementById('pause')
+var ff = getElementById('ff')
+var rew = getElementById('rew')
+var skipf = getElementById('skipf')
+var skipb = getElementById('skipb')
+var song = getElementById('song')
+var pause = getElementById('pause')
