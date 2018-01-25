02024da6ef08831b0ba2f654c31fef2306fabe4c
aciukurescu <andrewciukurescu@gmail.com>
2018-01-25 12:38:30 -0500

    play and pause starts and stops song

--- index.js
+++ index.js
@@ -1,47 +1,55 @@
-console.log('test')
+// console.log('test')
 
 
-var box = document.getElementById('box')
-var start = document.getElementById('play')
-var pause = document.getElementById('pause')
-var ff = document.getElementById('ff')
-var rew = document.getElementById('rew')
-var skipf = document.getElementById('skipf')
-var skipb = document.getElementById('skipb')
-var song = document.getElementById('song')
-var pause = document.getElementById('pause')
+// var box = document.getElementById('box')
+var start = document.getElementById('play');
+var pause = document.getElementById('pause');
+// var ff = document.getElementById('ff')
+// var rew = document.getElementById('rew')
+// var skipf = document.getElementById('skipf')
+// var skipb = document.getElementById('skipb')
+// var song = document.getElementById('song')
+// var pause = document.getElementById('pause')
+
 
 start.addEventListener('click', function(){
-	;
+	document.getElementById('myAudio').play();
 })
 
-var getaudio = $('#player')[0];
 
+pause.addEventListener('click', function(){
+	document.getElementById('myAudio').pause();
+})
 
-function Playlist(){
-	this.songs = []
-	this.addSongs = function(x){
-		this.songs.push(x)
-		console.log('A new song was added')
-	}
+// function Jukebox(){
+// 	this.playlist = [] // list of songs
+// 	this.addSong = function(x){
+// 		this.playlist.push(x);
+// 		console.log('A new song was added')
+// 	}
 
-	this.listSongs = function(x){
-		for (var i = 0; i < this.songs.length; i++){
-			console.log(this.songs[i].name + ' is on your playlist')
-		}
-	}
-}
+// 		//is this needed?
+// 	this.listSongs = function(x){
+// 		for (var i = 0; i < this.songs.length; i++){
+// 			console.log(this.songs[i].name + ' is on your playlist')
+// 		}
+// 	}
+// }
 
-class Song {
-	constructor (title, artist, location){
-		this.title = title;
-		this.artist = artist;
-		this.location = location;
-	}
-}
+// class Song {
+// 	constructor (title, artist, location){
+// 		this.title = title;
+// 		this.artist = artist;
+// 		this.location = location; // url/local location
+// 	}
+// }
 
-var brethren = new Song ('Brethren Arise', 'Chris Zabriskie', 'audio.Brethren_Arise.mp3')
-var outlet = new Song ('Outlet', 'Silent Partner', 'audio.Outlet.mp3')
+// var brethren = new Song ('Brethren Arise', 'Chris Zabriskie', 'audio/Brethren_Arise.mp3')
+// var outlet = new Song ('Outlet', 'Silent Partner', 'audio/Outlet.mp3')
+// var playlist = new Jukebox
+
+// playlist.addSong(brethren);
+// playlist.addSong(outlet);
 
 
 
