6176854139e18ce65b1de184a07565ca0ca7aa0d
aciukurescu <andrewciukurescu@gmail.com>
2018-01-25 17:50:12 -0500

    buttons work music added

--- index.js
+++ index.js
@@ -1,55 +1,36 @@
-// console.log('test')
 
+var audio = new Audio();
+i = 0;
 
-// var box = document.getElementById('box')
-var start = document.getElementById('play');
-var pause = document.getElementById('pause');
-// var ff = document.getElementById('ff')
-// var rew = document.getElementById('rew')
-// var skipf = document.getElementById('skipf')
-// var skipb = document.getElementById('skipb')
-// var song = document.getElementById('song')
-// var pause = document.getElementById('pause')
+// // Button Function
+var playbtn = document.getElementById('play');
+var pausebtn = document.getElementById('pause');
+var skipbtn = document.getElementById('skipf');
 
+// Setting Playlist
+var playlist = ["audio/song2.mp3","audio/song3.mp3", "audio/song4.mp3"];
+audio.src = playlist[i];
 
-start.addEventListener('click', function(){
-	document.getElementById('myAudio').play();
+// Press play music begin
+playbtn.addEventListener('click', function(){
+	audio.play();
+})
+
+// Press pause music pauses
+pausebtn.addEventListener('click', function(){
+	audio.pause();
 })
 
 
-pause.addEventListener('click', function(){
-	document.getElementById('myAudio').pause();
+skipbtn.addEventListener('click', function(){
+	if ( i === playlist.length-1){
+		i = 0
+		audio.src = playlist[i];
+		audio.play()
+	} else {
+		i++
+		audio.src = playlist[i];
+		audio.play()
+	}
 })
-
-// function Jukebox(){
-// 	this.playlist = [] // list of songs
-// 	this.addSong = function(x){
-// 		this.playlist.push(x);
-// 		console.log('A new song was added')
-// 	}
-
-// 		//is this needed?
-// 	this.listSongs = function(x){
-// 		for (var i = 0; i < this.songs.length; i++){
-// 			console.log(this.songs[i].name + ' is on your playlist')
-// 		}
-// 	}
-// }
-
-// class Song {
-// 	constructor (title, artist, location){
-// 		this.title = title;
-// 		this.artist = artist;
-// 		this.location = location; // url/local location
-// 	}
-// }
-
-// var brethren = new Song ('Brethren Arise', 'Chris Zabriskie', 'audio/Brethren_Arise.mp3')
-// var outlet = new Song ('Outlet', 'Silent Partner', 'audio/Outlet.mp3')
-// var playlist = new Jukebox
-
-// playlist.addSong(brethren);
-// playlist.addSong(outlet);
-
-
 
